0a56a86c7997448bef19edcf37930d6d47731055
Andrew Hartwig <andrew.hartwig1@gmail.com>
2023-09-15 15:25:12 -0400

    feature: pwa
    feat: gh-deploy
    
    update: release gap illustration mode
    
    deploy: hdpi fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AutoSolverProvider } from './context/AutoSolverContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AutoSolverProvider>
         <App />
       </AutoSolverProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
